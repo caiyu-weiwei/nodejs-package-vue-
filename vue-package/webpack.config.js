6b936524a96ffb30b84830d998e789cc3d162980
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2019-03-06 15:20:39 +0800

    webpack.config.js package.json

--- vue-package/webpack.config.js
+++ vue-package/webpack.config.js
@@ -1,12 +1,19 @@
 var path = require('path')
 var webpack = require('webpack')
+const NODE_ENV = process.env.NODE_ENV
+
+console.log('NODE_ENV', NODE_ENV)
 
 module.exports = {
-  entry: './src/main.js',
+  // 根据不同的执行环境配置不同的入口
+  entry: NODE_ENV === 'development' ? './src/main.js' : './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: 'cySwitch.js',
+    library: 'cySwitch', // 指定的就是你使用require时的模块名
+    libraryTarget: 'umd',  // libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的
+    umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
   },
   module: {
     rules: [
